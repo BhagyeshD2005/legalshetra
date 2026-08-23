0403ab2f6dea856673eddad88ba3cc35b5273607
BhagyeshD2005 <146364017+BhagyeshD2005@users.noreply.github.com>
2026-08-23 19:35:28 +0530

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,5 +3,5 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'gemini-3.1-flash-lite',
+  model: 'gemini-3.1-flash-live-preview',
 });
